29f86e35f27a3e05f4d0d15c0b823452429ee434
Kevin dsa <67777018+kevin46dsa@users.noreply.github.com>
2022-08-06 16:48:17 -0400

    Created Clock app

---
+++ src/clock.js
@@ -0,0 +1,40 @@
+import React, {  useState, useEffect } from 'react';
+import '../src/clock.css';
+
+
+
+
+const Clock = () => {
+
+const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+useEffect(() => {
+    setTimeout(() => {
+        updateClock();
+      }, 1000);
+  });
+
+
+function updateClock(){
+setTime(
+new Date().toLocaleTimeString()
+);
+}
+
+return (
+    <div className="container">
+    <div className="row">
+      
+      <div className="col">
+      <div className="Time">
+        <p> {time}</p>
+        </div>
+        
+      </div>
+     
+    </div>
+  </div> 
+
+);
+}
+export default Clock;
